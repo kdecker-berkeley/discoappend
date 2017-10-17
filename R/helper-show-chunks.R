74cf8adb3bfcda096fd2b57815784dcccd551360
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2017-10-17 11:20:43 -0400

    add helper for displaying available chunks and outputs

---
+++ R/helper-show-chunks.R
@@ -0,0 +1,25 @@
+chunk_df <- function() {
+  chunk_finder <- readr::read_csv("R:/Prospect Development/Prospect Analysis/discoappend/extdata/chunk_finder.csv")
+}
+
+#' Browse the available chunk names and outputs
+#'
+#' Pops up an interactive table that allows you to sort and search through a
+#' listing of all available chunks with outputs for each
+#'
+
+#' @export
+show_chunks <- function() {
+  if (!requireNamespace("DT", quietly = TRUE)) {
+    stop('DT package needed for show_widgets to work.\n',
+         'To install: install.packages("DT")',
+         call. = FALSE)
+  }
+
+  chunk_list <- chunk_df()
+  DT::datatable(chunk_list, rownames = FALSE,
+                options = list(
+                  order = list(list(1, "asc"))
+                ))
+}
+
