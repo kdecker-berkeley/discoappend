9fffa471f68cae1a020e2a52c93cf2d765749fde
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2018-03-20 10:50:40 -0400

    documentation

--- R/chunk-degrees.R
+++ R/chunk-degrees.R
@@ -1,7 +1,7 @@
 #' Append degree data to a discoveryengine definition
 #'
 #' @param constituency A discoveryengine definition
-#' @return data frame with the following columns: entity id, undergrad degree, graduate degree, degrees, class campaign year, spouse undergraduate degree, spouse graduate degree, spouse degrees, spouse class campaign year
+#' @return data frame with the following columns: entity id, undergrad degree, graduate degree, degrees, preferred school code, preferred school, class campaign year, spouse undergraduate degree, spouse graduate degree, spouse degrees, spouse class campaign year
 #' @rdname degrees
 #' @export
 #' @examples
