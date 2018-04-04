47c54c1eda031b6d33769602f62a08da9ff9cc07
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2018-04-04 08:57:29 -0400

    add custom function for CA giving

---
+++ R/ca-giving.R
@@ -0,0 +1,62 @@
+#' Detailed CA campaign giving summaries in custom chunks
+#'
+#' These functions are used inside the \code{\link{custom}} function, to create
+#' detailed summaries of California state level political contributions. They have a similar
+#' interface to the ca_gave_to_* widgets in the Discovery Engine.
+#'
+#' @param ... One or more codes (candidate/proposition)
+#' @param from Start date, in the format YYYYMMDD
+#' @param to End date, in the format YYYYMMDD
+#'
+#' @seealso \code{\link{custom}}
+#'
+#' @examples
+#' wealthy = has_capacity(1)
+#'
+#' ## summary of all giving to CA state level campaigns to candidates
+#' wealthy %>%
+#'   custom(
+#'     ca_giving = ca_giving_to_candidate()
+#'   )
+#'
+#' ## find donors to Kamala Harris (whose code is CA770144) and append amounts
+#' ca_gave_to_candidate(CA770144) %>%
+#' custom(
+#' harris_giving = ca_giving_to_candidate(CA770144)
+#' )
+#'
+#' @name custom_ca_giving
+NULL
+
+#' @rdname custom_ca_giving
+#' @export
+ca_giving_to_proposition <- function(..., from = NULL, to = NULL) {
+  res <- discoveryengine::ca_gave_to_proposition(..., from = from, to = to)
+  output <- "sum(amount)"
+
+  build_chunk(
+    res,
+    output = output,
+    isgrouped = TRUE,
+    fmt = na_zero,
+    household = TRUE,
+    summarizer = "sum"
+  )
+
+}
+
+
+#' @rdname custom_ca_giving
+#' @export
+ca_giving_to_candidate <- function(..., from = NULL, to = NULL) {
+  res <- discoveryengine::ca_gave_to_candidate(..., from = from, to = to)
+  output <- "sum(amount)"
+
+  build_chunk(
+    res, output = output,
+    isgrouped = TRUE,
+    fmt = na_zero,
+    household = TRUE,
+    summarizer = "sum"
+  )
+}
