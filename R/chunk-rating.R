fa8f3fdb3cefcd56afa6a06eef251d887b244e1e
cwolfsonseeley <cwolfsonseeley@berkeley.edu>
2019-05-14 09:43:01 -0400

    change major gift score to gift score

--- R/chunk-rating.R
+++ R/chunk-rating.R
@@ -1,7 +1,7 @@
 #' Append rating data to a discoveryengine definition
 #'
 #' @param constituency A discoveryengine definition
-#' @return data frame with the following columns: entity id, capacity rating, capacity rating date, implied capacity score, implied capacity description, implied capacity score date, major gift score, major gift description, major gift score date, CNR score, CNR score description, CNR score date, Gift Planning score, Gift Planning score description, Gift Planning score date, Engineering score, Engineering score description, Engineering score date, Haas score, Haas score description, Haas score date
+#' @return data frame with the following columns: entity id, capacity rating, capacity rating date, implied capacity score, implied capacity description, implied capacity score date, gift score, gift score description, gift score date, CNR score, CNR score description, CNR score date, Gift Planning score, Gift Planning score description, Gift Planning score date, Engineering score, Engineering score description, Engineering score date, Haas score, Haas score description, Haas score date
 #' @rdname rating
 #' @export
 #' @examples
@@ -18,7 +18,7 @@ rating <- function(constituency) {
       column_formats = list(implied_capacity_desc = model_desc_format)) %>%
     listbuilder::add_template(
       mgs_template,
-      column_formats = list(major_gift_score_desc = model_desc_format)) %>%
+      column_formats = list(gift_score_desc = model_desc_format)) %>%
     listbuilder::add_template(
       cnr_model_template,
       column_formats = list(cnr_score_desc = model_desc_format)) %>%
